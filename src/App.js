45f54a991c4282cf0ba217087dacc4289d12176c
Aftab Alam <alam69745@gmail.com>
2024-01-06 20:51:57 +0530

    card component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Card from "./components/card"
 export default function App(){
   return (
     <div>
-      
+      <Card />
     </div>
   )
 }
---
+++ src/components/card/index.jsx
@@ -0,0 +1,3 @@
+export default function Card(){
+    return <div style={{width: "320px", height: "320px", border: "1px solid red"}}></div>
+}
