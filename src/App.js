f7e67b4ab168c5758fbf8486becc1e4ed2957d6b
Aftab Alam <alam69745@gmail.com>
2024-01-06 21:54:20 +0530

    navbar component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Navbar from "./components/navbar"
 export default function App(){
   return (
     <div>
-      
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css"
+export default function Navbar(){
+    return (
+        <div class="topnav">
+              <a class="active" href="#home">Home</a>
+              <a href="#news">About</a>
+              <a href="#contact">Contact</a>
+              <a href="#about">Services</a>
+              <a href="#about">Login</a>
+        </div>
+    )
+}
