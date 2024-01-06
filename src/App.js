79444a81e62dd355e91b6d29447361ab411da6ca
Aftab Alam <alam69745@gmail.com>
2024-01-06 21:30:08 +0530

    day component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Day from "./components/day"
 export default function App(){
   return (
     <div>
-      
+      <Day />
     </div>
   )
 }
---
+++ src/components/day/index.jsx
@@ -0,0 +1,5 @@
+export default function Day(){
+    const d = new Date()
+    const day = d.getDay()
+    return <p>{day}</p>
+}
