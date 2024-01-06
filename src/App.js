a64d0f84ae8956b5252cc9820443571ef7edf526
Aftab Alam <alam69745@gmail.com>
2024-01-06 21:42:43 +0530

    list component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import List from "./components/list"
 export default function App(){
   return (
     <div>
-      
+      <List />
     </div>
   )
 }
---
+++ src/components/list/index.jsx
@@ -0,0 +1,11 @@
+export default function List(){
+    return (
+        <div>
+            <ul style={{listStyle: "none"}}>
+                <li>Home</li>
+                <li>About-us</li>
+                <li>Contact-us</li>
+            </ul>
+        </div>
+    )
+}
